82c9077d9c17907f2ccfb23634f43112073416f9
elandyg <andrewgagne@outlook.com>
2019-12-03 20:12:12 -0500

    set up individual funcs and styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./Header.css";
+import "./Scoreboard.css";
+import "./Result.css";
+import "./Choice.css";
+
+const Choices = props => (
+  <div className="choices">
+    {props.choices.map(choice => (
+      <Choice key={choice} choice={choice} />
+    ))}
+  </div>
+);
+
+function Choice(props) {
+  return (
+    <div className="choice">
+      <img src={`images/${props.choice}.png`} alt="" />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Rock Paper Scissors</h1>
+    </header>
+  );
+}
+
+function Scoreboard() {
+  return (
+    <div className="score-board">
+      <div id="user-label" className="badge">
+        user
+      </div>
+      <div id="computer-label" className="badge">
+        comp
+      </div>
+      <span id="user-score">0</span>:<span id="computer-score">0</span>
+    </div>
+  );
+}
+
+function Result() {
+  return (
+    <div className="result">
+      <p>Paper covers rock. You win!</p>
+    </div>
+  );
+}
 
 function App() {
+  const options = ["rock", "paper", "scissors"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Scoreboard />
+      <Result />
+      <Choices choices={options} />
+      <p>Make your move!</p>
     </div>
   );
 }
